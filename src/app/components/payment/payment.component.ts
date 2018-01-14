a698737ebd33491ec58411c418c1d82cce36c86a
pipatng <besamato@gmail.com>
2018-01-14 22:33:07 +0700

    Change https request

--- src/app/components/payment/payment.component.ts
+++ src/app/components/payment/payment.component.ts
@@ -10,10 +10,30 @@ declare var $: any;
 export class PaymentComponent implements OnInit {
 
   datatable;
+  years = [];
+  months = [
+    { 'id' : 1 , 'name': 'January'},
+    { 'id' : 2 , 'name': 'Febuary'},
+    { 'id' : 3, 'name': 'March'},
+    { 'id' : 4, 'name': 'April'},
+    { 'id' : 5, 'name': 'May'},
+    { 'id' : 6, 'name': 'June'},
+    { 'id' : 7, 'name': 'July'},
+    { 'id' : 8, 'name': 'August'},
+    { 'id' : 9, 'name': 'September'},
+    { 'id' : 10, 'name': 'October'},
+    { 'id' : 11, 'name': 'November'},
+    { 'id' : 12 , 'name': 'December'}
+  ];
 
   constructor() { }
 
   ngOnInit() {
+    const startYear = 2018;
+    const thisYear = new Date().getFullYear();
+    for (let i = startYear; i <= thisYear; i++) {
+      this.years.push(i);
+    }
   }
 
   ngAfterViewInit() {
