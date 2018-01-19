32faa5ff5e79f2aeda92c97aef30763f0e2f2ae1
pipatng <besamato@gmail.com>
2018-01-20 01:05:05 +0700

    Add market map

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  rows = new Array();
+  cols = new Array();
+
   constructor() { }
 
   ngOnInit() {
+    const startText = 'A';
+    for (let i = 0; i < 20; i++) {
+      this.cols.push(String.fromCharCode((startText.charCodeAt(0) + i)));
+    }
+
+    for (let i = 1; i < 10; i++) {
+      this.rows.push(i);
+    }
   }
 
 }
